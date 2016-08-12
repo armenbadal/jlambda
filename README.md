7208ca04dfa09d6931e791c892a6695363016419
Արմեն Բադալյան <armenbadal@gmail.com>
2016-08-12 14:45:44 +0400

    Grammar

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # jlambda
 
 Լամբդա լեզվի իրականացում։
+
+````
+Expr = Constant
+     | Ident
+     | '(' Expr ')'
+     | Oper Expr
+     | Expr Oper Expr
+     | 'if' Expr 'then' Expr 'else' Expr
+     | 'lambda' Ident '.' Expr
+     | 'apply' Expr 'to' Expr
+     | '#' Ident '.' Expr
+     | '{' Expr '?' Expr ':' Expr '}'
+````
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -2,23 +2,6 @@ package parser;
 
 import interpreter.*;
 
-/**
- * Grammar:
- *
- * Expr = Constant
- *      | Ident
- *      | '(' Expr ')'
- *      | Oper Expr
- *      | Expr Oper Expr
- *      | 'if' Expr 'then' Expr 'else' Expr
- *      | 'lambda' Ident '.' Expr
- *      | 'apply' Expr 'to' Expr
- *
- *      | '#' Ident '.' Expr
- *      | '{' Expr '?' Expr ':' Expr '}'
- *      | '[' Expr '|' Expr ']'.
- */
-
 /**/
 public class Parser {
     private Scanner scanner = null;
